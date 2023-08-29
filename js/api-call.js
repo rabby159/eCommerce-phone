68384202de4f539bbdb37661a6c1bc08d821b837
Md Rabby <ablzerabbi159@gmail.com>
2023-08-29 17:59:50 +0600

    footer added

--- js/api-call.js
+++ js/api-call.js
@@ -1,1 +1,8 @@
-console.log('check')
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones);
+};
+
+loadData();
